58db2539ec6d2f243258a1a0f38e6ac44ee3b806
john gallagher <johnnygfromdonegal@gmail.com>
2022-09-25 16:48:26 +0100

    Added LinkedListCycle

---
+++ Easy/LinkedListCycle.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # floyd's Tortoise & Hare
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+# if __name__ == "__main__":
+#     sol = Solution()
