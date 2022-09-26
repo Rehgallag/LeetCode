75fc89d3478d4d66c90e3c4a831b1dd28c3a4020
John Gallagher <johnnygfromdonegal@gmail.com>
2022-09-26 13:10:59 +0100

    Added 2 easy tree problems

---
+++ Easy/InverBinaryTree.py
@@ -0,0 +1,26 @@
+from turtle import right
+from typing import List, Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [2,1,3]
+    print(sol.invertTree(nums))
