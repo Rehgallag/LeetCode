f1b7d14e6fe2181a1d344885e4de4deefa93dcf2
John Gallagher <johnnygfromdonegal@gmail.com>
2022-09-27 15:13:49 +0100

    3 new easy qs

---
+++ Easy/SameTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
+
+# if __name__ == "__main__":
+#     sol = Solution()
+#     p, q = [1,2,3], [1,2,3]
+#     print(sol.isSameTree(p,q))
