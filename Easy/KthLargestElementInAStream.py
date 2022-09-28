405357fe8f0e8b7101c3da488fa1b46b888d142e
John Gallagher <johnnygfromdonegal@gmail.com>
2022-09-28 13:23:24 +0100

    Added empty KthLargest file

---
+++ Easy/KthLargestElementInAStream.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        return
+
+    def add(self, val: int) -> int:
+        return 1
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+# if __name__ == "__main__":
+#     sol = Solution()
